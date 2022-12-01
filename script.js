0eefd5e1779771c4da3d40124ab721948aaa33d3
Guilherme Silveira <108364424+SilveiraGuilherme@users.noreply.github.com>
2022-12-01 16:33:19 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,108 @@
+function playerSelectionValidator() {
+  playerSelection = prompt(
+    "Make your choice: Rock, Paper or Scissors?"
+  ).toLowerCase();
+
+  switch (playerSelection) {
+    case "rock":
+      break;
+    case "paper":
+      break;
+    case "scissors":
+      break;
+    default:
+      alert("Oops... Not a valid option!");
+      playerSelectionValidator();
+  }
+  return playerSelection;
+}
+
+function computerPlay() {
+  let n = Math.random();
+  n < 0.34
+    ? (computerSelection = "rock")
+    : n < 0.67
+    ? (computerSelection = "paper")
+    : (computerSelection = "scissors");
+  return computerSelection;
+}
+
+function rockPaperScissors() {
+  alert("Press OK to start");
+  let playerWins = 0;
+  let computerWins = 0;
+
+  for (let i = 0; i < 5; i++) {
+    playerSelectionValidator();
+    console.log(`Player selection is: ${playerSelection}`);
+
+    computerPlay();
+    console.log(`Computer selection is: ${computerSelection}`);
+
+    attemptMsg = `Attempt ${i + 1} of 5
+    You: ${playerSelection}  x  Computer: ${computerSelection}\n`;
+
+    if (playerSelection !== computerSelection) {
+      if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+          playerWins++;
+          alert(
+            `${attemptMsg}\nYou got it, ${playerSelection} beats ${computerSelection}.\n    You ${playerWins}  x  Computer ${computerWins}`
+          );
+        } else {
+          computerWins++;
+          alert(
+            `${attemptMsg}\nPoor you, ${computerSelection} beats ${playerSelection}.\n    You ${playerWins}  x  Computer ${computerWins}`
+          );
+        }
+      } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+          playerWins++;
+          alert(
+            `${attemptMsg}\nYou got it, ${playerSelection} beats ${computerSelection}.\n    You ${playerWins}  x  Computer ${computerWins}`
+          );
+        } else {
+          computerWins++;
+          alert(
+            `${attemptMsg}\nPoor you, ${computerSelection} beat ${playerSelection}.\n    You ${playerWins}  x  Computer ${computerWins}`
+          );
+        }
+      } else if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+          playerWins++;
+          alert(
+            `${attemptMsg}\nYou got it, ${playerSelection} beat ${computerSelection}.\n    You ${playerWins}  x  Computer ${computerWins}`
+          );
+        } else {
+          computerWins++;
+          alert(
+            `${attemptMsg}\nPoor you, ${computerSelection} beats ${playerSelection}.\n    You ${playerWins}  x  Computer ${computerWins}`
+          );
+        }
+      }
+    } else
+      alert(
+        `${attemptMsg}\nIt's a tie! Nobody wins.\n    You ${playerWins}  x  Computer ${computerWins}`
+      );
+    console.log(`Player ${playerWins} x Computer ${computerWins}`);
+  }
+
+  if (playerWins > computerWins) {
+    alert(
+      `Congrats! You are the winner.\nYou ${playerWins}  x  Computer ${computerWins}`
+    );
+    console.log("Congrats! You are the winner.");
+  } else if (computerWins > playerWins) {
+    alert(
+      `Better luck next time! :(\nYou ${playerWins}  x  Computer ${computerWins}`
+    );
+    console.log("Better luck next time! :(");
+  } else {
+    alert(
+      `Guess what!? We don't have a winner!\nYou ${playerWins}  x  Computer ${computerWins}`
+    );
+    console.log("Guess what!? We don't have a winner!");
+  }
+}
+
+rockPaperScissors();

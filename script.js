20979db41cec92d5a0adfd4eeb8a77a377af9f5b
Guilherme Silveira <108364424+SilveiraGuilherme@users.noreply.github.com>
2022-12-02 12:00:26 -0300

    Added actions in case user clicks on cancel button

--- script.js
+++ script.js
@@ -1,46 +1,4 @@
-function playerSelectionValidator() {
-  playerSelection = prompt(
-    "Make your choice: Rock, Paper or Scissors?"
-  ).toLowerCase();
-
-  switch (playerSelection) {
-    case "rock":
-      break;
-    case "paper":
-      break;
-    case "scissors":
-      break;
-    default:
-      alert("Oops... Not a valid option!");
-      playerSelectionValidator();
-  }
-  return playerSelection;
-}
-
-function computerPlay() {
-  let n = Math.random();
-  n < 0.34
-    ? (computerSelection = "rock")
-    : n < 0.67
-    ? (computerSelection = "paper")
-    : (computerSelection = "scissors");
-  return computerSelection;
-}
-
-function playAgain() {
-  restart = prompt("Do you want to play again? (Y / N)").toLowerCase();
-  switch (restart) {
-    case "y":
-      rockPaperScissors();
-      break;
-    case "n":
-      alert("See you later!");
-      break;
-    default:
-      alert("I didn't get that. Try again!");
-      playAgain();
-  }
-}
+rockPaperScissors();
 
 function rockPaperScissors() {
   alert("Press OK to start");
@@ -49,57 +7,62 @@ function rockPaperScissors() {
 
   for (let i = 0; i < 5; i++) {
     playerSelectionValidator();
-    console.log(`Player selection is: ${playerSelection}`);
 
-    computerPlay();
-    console.log(`Computer selection is: ${computerSelection}`);
+    if (playerSelection === null) {
+      return alert("Ok. I got it. You don't want to play.");
+    } else {
+      console.log(`Player selection is: ${playerSelection}`);
 
-    attemptMsg = `Attempt ${i + 1} of 5
-    You: ${playerSelection}  x  Computer: ${computerSelection}\n`;
+      computerPlay();
+      console.log(`Computer selection is: ${computerSelection}`);
 
-    if (playerSelection !== computerSelection) {
-      if (playerSelection === "rock") {
-        if (computerSelection === "scissors") {
-          playerWins++;
-          alert(
-            `${attemptMsg}\nYou got it, ${playerSelection} beats ${computerSelection}.\n    You ${playerWins}  x  Computer ${computerWins}`
-          );
-        } else {
-          computerWins++;
-          alert(
-            `${attemptMsg}\nPoor you, ${computerSelection} beats ${playerSelection}.\n    You ${playerWins}  x  Computer ${computerWins}`
-          );
+      attemptMsg = `Attempt ${i + 1} of 5
+      You: ${playerSelection}  x  Computer: ${computerSelection}\n`;
+
+      if (playerSelection !== computerSelection) {
+        if (playerSelection === "rock") {
+          if (computerSelection === "scissors") {
+            playerWins++;
+            alert(
+              `${attemptMsg}\nYou got it, ${playerSelection} beats ${computerSelection}.\n      You ${playerWins}  x  Computer ${computerWins}`
+            );
+          } else {
+            computerWins++;
+            alert(
+              `${attemptMsg}\nPoor you, ${computerSelection} beats ${playerSelection}.\n      You ${playerWins}  x  Computer ${computerWins}`
+            );
+          }
+        } else if (playerSelection === "paper") {
+          if (computerSelection === "rock") {
+            playerWins++;
+            alert(
+              `${attemptMsg}\nYou got it, ${playerSelection} beats ${computerSelection}.\n      You ${playerWins}  x  Computer ${computerWins}`
+            );
+          } else {
+            computerWins++;
+            alert(
+              `${attemptMsg}\nPoor you, ${computerSelection} beat ${playerSelection}.\n      You ${playerWins}  x  Computer ${computerWins}`
+            );
+          }
+        } else if (playerSelection === "scissors") {
+          if (computerSelection === "paper") {
+            playerWins++;
+            alert(
+              `${attemptMsg}\nYou got it, ${playerSelection} beat ${computerSelection}.\n      You ${playerWins}  x  Computer ${computerWins}`
+            );
+          } else {
+            computerWins++;
+            alert(
+              `${attemptMsg}\nPoor you, ${computerSelection} beats ${playerSelection}.\n      You ${playerWins}  x  Computer ${computerWins}`
+            );
+          }
         }
-      } else if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
-          playerWins++;
-          alert(
-            `${attemptMsg}\nYou got it, ${playerSelection} beats ${computerSelection}.\n    You ${playerWins}  x  Computer ${computerWins}`
-          );
-        } else {
-          computerWins++;
-          alert(
-            `${attemptMsg}\nPoor you, ${computerSelection} beat ${playerSelection}.\n    You ${playerWins}  x  Computer ${computerWins}`
-          );
-        }
-      } else if (playerSelection === "scissors") {
-        if (computerSelection === "paper") {
-          playerWins++;
-          alert(
-            `${attemptMsg}\nYou got it, ${playerSelection} beat ${computerSelection}.\n    You ${playerWins}  x  Computer ${computerWins}`
-          );
-        } else {
-          computerWins++;
-          alert(
-            `${attemptMsg}\nPoor you, ${computerSelection} beats ${playerSelection}.\n    You ${playerWins}  x  Computer ${computerWins}`
-          );
-        }
-      }
-    } else
-      alert(
-        `${attemptMsg}\nIt's a tie! Nobody wins.\n    You ${playerWins}  x  Computer ${computerWins}`
-      );
-    console.log(`Player ${playerWins} x Computer ${computerWins}`);
+      } else
+        alert(
+          `${attemptMsg}\nIt's a tie! Nobody wins.\n      You ${playerWins}  x  Computer ${computerWins}`
+        );
+      console.log(`Player ${playerWins} x Computer ${computerWins}`);
+    }
   }
 
   if (playerWins > computerWins) {
@@ -121,4 +84,54 @@ function rockPaperScissors() {
   playAgain();
 }
 
-rockPaperScissors();
+function playerSelectionValidator() {
+  playerSelection = prompt("Make your choice: Rock, Paper or Scissors?");
+
+  if (playerSelection === null) {
+    return;
+  } else {
+    playerSelection = playerSelection.toLowerCase();
+    switch (playerSelection) {
+      case "rock":
+        break;
+      case "paper":
+        break;
+      case "scissors":
+        break;
+      default:
+        alert("Oops... Not a valid option!");
+        playerSelectionValidator();
+    }
+    return playerSelection;
+  }
+}
+
+function computerPlay() {
+  let n = Math.random();
+  n < 0.34
+    ? (computerSelection = "rock")
+    : n < 0.67
+    ? (computerSelection = "paper")
+    : (computerSelection = "scissors");
+  return computerSelection;
+}
+
+function playAgain() {
+  restart = prompt("Do you want to play again? (Y / N)");
+  if (restart === null) {
+    return;
+  } else {
+    restart = restart.toLowerCase();
+    switch (restart) {
+      case "y":
+        rockPaperScissors();
+        break;
+      case "n":
+        alert("See you later!");
+        break;
+      default:
+        alert("I didn't get that. Try again!");
+        playAgain();
+    }
+  }
+}
